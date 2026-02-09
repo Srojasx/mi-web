0fdb5fe3d319ccc0f9cfd87afb12d2f1867c4e03
Srojasx <santi_rg20@hotmail.com>
2026-02-08 19:09:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("miBoton");
+
+boton.addEventListener("click", function () {
+  alert("¡Bienvenido! Ya estoy usando JavaScript 🚀");
+  boton.textContent = "Hecho 😎";
+});
